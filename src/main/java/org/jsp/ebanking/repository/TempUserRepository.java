43ae1049de67051ef04c7328253e61e1ada26bd0
saishkulkarni <saishkulkarni7@gmail.com>
2025-11-04 10:44:40 +0530

    Registration - 1

---
+++ src/main/java/org/jsp/ebanking/repository/TempUserRepository.java
@@ -0,0 +1,8 @@
+package org.jsp.ebanking.repository;
+
+import org.jsp.ebanking.entity.TempUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TempUserRepository extends CrudRepository<TempUser, String> {
+
+}
--- src/main/java/org/jsp/ebanking/service/UserService.java
+++ src/main/java/org/jsp/ebanking/service/UserService.java
@@ -1,5 +1,13 @@
 package org.jsp.ebanking.service;
 
+import org.jsp.ebanking.dto.UserDto;
+import org.jsp.ebanking.entity.TempUser;
+import org.springframework.http.ResponseEntity;
+
 public interface UserService {
+
+	ResponseEntity<TempUser> register(UserDto dto);
+
+	ResponseEntity<TempUser> fetch(String email);
 
 }
