11a8ed263fba17a5645e2658dd660e43813d7c08
saishkulkarni <saishkulkarni7@gmail.com>
2025-11-05 10:42:15 +0530

    Redis Service Completed

--- src/main/java/org/jsp/ebanking/exception/GlobalExceptionHandler.java
+++ src/main/java/org/jsp/ebanking/exception/GlobalExceptionHandler.java
@@ -19,4 +19,9 @@ public class GlobalExceptionHandler {
 		return ResponseEntity.status(400).body(new ErrorDto(errorMessages));
 	}
 
+	@ExceptionHandler(DataExistsException.class)
+	public ResponseEntity<Object> handle(DataExistsException exception) {
+		return ResponseEntity.status(409).body(new ErrorDto(exception.getMessage()));
+	}
+
 }
--- src/main/java/org/jsp/ebanking/repository/TempUserRepository.java
+++
@@ -1,8 +0,0 @@
-package org.jsp.ebanking.repository;
-
-import org.jsp.ebanking.entity.TempUser;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TempUserRepository extends CrudRepository<TempUser, String> {
-
-}
