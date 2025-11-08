cda608e0111167c221fc3b77d2591fa9f6e14151
saishkulkarni <saishkulkarni7@gmail.com>
2025-11-08 10:52:10 +0530

    CI-CD Implemented

--- src/test/java/org/jsp/ebanking/EbankingApplicationTests.java
+++ src/test/java/org/jsp/ebanking/EbankingApplicationTests.java
@@ -1,13 +1,9 @@
 package org.jsp.ebanking;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EbankingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
 }
+
